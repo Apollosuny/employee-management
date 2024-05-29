e1c8030a7208eefadb0f44db20673282b186fc16
trungtran <baotrung06092003@gmail.com>
2024-05-29 20:34:46 +0700

    fix: UI employee page

--- app/views/client/dashboard.php
+++ app/views/client/dashboard.php
@@ -1,31 +1,152 @@
 <div class="container" style="min-height: 100vh;">
     <div class="d-flex justify-content-center mt-5">
-        <div class="row">
-            <div class="col-6 d-flex justify-content-center">
-                <a href="<?php echo _WEB_ROOT; ?>/dashboard/profile" class="card text-center mb-3" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">Profile</h5>
+        <div class="row d-flex justify-content-between">
+            <div class="col-6 max-height-350">
+                <div class="bg-body-tertiary profile-wrapper h-100">
+                    <div class="" style="margin-bottom: 24px">
+                        <h2>Employee's information</h2>
                     </div>
-                </a>
-            </div>
-            <div class="col-6 d-flex justify-content-center">
-                <a href="<?php echo _WEB_ROOT; ?>/dashboard/contracts" class="card text-center mb-3" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">Contracts</h5>
+                    <div class="d-flex justify-content-between align-items-center g-20">
+                        <div class="d-flex flex-column justify-content-center align-items-center g-10">
+                            <img src="https://htmediagroup.vn/wp-content/uploads/2022/08/Anh-cong-so-1-min.jpg"
+                                alt="Avatar" class="profile-avatar" />
+                            <div>
+                                <a href="<?php echo _WEB_ROOT; ?>/dashboard/profile">View detail</a>
+                            </div>
+                        </div>
+                        <div class="d-flex flex-column g-20">
+                            <div class="d-flex align-items-center g-10">
+                                <div class="">Employee's id:</div>
+                                <div class="fw-semibold">NV0001</div>
+                            </div>
+                            <div class="d-flex align-items-center g-10">
+                                <div class="">Fullname:</div>
+                                <div class="fw-semibold">John Smith</div>
+                            </div>
+                            <div class="d-flex align-items-center g-10">
+                                <div class="">Gender:</div>
+                                <div class="fw-semibold">Male</div>
+                            </div>
+                            <div class="d-flex align-items-center g-10">
+                                <div class="">Date of birth:</div>
+                                <div class="fw-semibold">NV0001</div>
+                            </div>
+                            <div class="d-flex align-items-center g-10">
+                                <div class="">Address:</div>
+                                <div class="fw-semibold">NV0001</div>
+                            </div>
+                        </div>
                     </div>
-                </a>
+                </div>
             </div>
-            <div class="col-6 d-flex justify-content-center">
-                <a href="<?php echo _WEB_ROOT; ?>/dashboard/attendanceChecking" class="card text-center mb-3" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">Attendance checking</h5>
+            <div class="col-6 max-height-350">
+                <div class="bg-body-tertiary profile-wrapper h-100">
+                    <div class="d-flex justify-content-between align-items-center" style="margin-bottom: 24px">
+                        <h2>Contracts</h2>
+                        <div>
+                            <a href="<?php echo _WEB_ROOT; ?>/dashboard/contracts">View detail</a>
+                        </div>
                     </div>
-                </a>
+                    <div class="">
+                        <div class="d-flex flex-column g-20">
+                            <div class="d-flex align-items-center g-10">
+                                <div class="">Salary:</div>
+                                <div class="fw-semibold">$1000</div>
+                            </div>
+                            <div class="d-flex align-items-center g-10">
+                                <div class="">Signed Date</div>
+                                <div class="fw-semibold">John Smith</div>
+                            </div>
+                            <div class="d-flex align-items-center g-10">
+                                <div class="">Start Date</div>
+                                <div class="fw-semibold">Male</div>
+                            </div>
+                            <div class="d-flex align-items-center g-10">
+                                <div class="">End Date</div>
+                                <div class="fw-semibold">NV0001</div>
+                            </div>
+                            <div class="d-flex align-items-center g-10">
+                                <div class="">Position</div>
+                                <div class="fw-semibold">NV0001</div>
+                            </div>
+                            <div class="d-flex align-items-center g-10">
+                                <div class="">Contract type</div>
+                                <div class="fw-semibold">NV0001</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="col-6 d-flex justify-content-center">
-                <div class="card text-center mb-3" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">Settings</h5>
+            <div class="col-6 max-height-350 mt-5">
+                <div class="bg-body-tertiary profile-wrapper h-100">
+                    <div class="d-flex justify-content-between align-items-center" style="margin-bottom: 24px">
+                        <h2>Attendance Checking</h2>
+                        <div>
+                            <a href="<?php echo _WEB_ROOT; ?>/dashboard/attendanceChecking">View detail</a>
+                        </div>
+                    </div>
+                    <div class="">
+                        <div class="d-flex flex-column g-20">
+                            <div class="d-flex align-items-center g-10">
+                                <div class="">Salary:</div>
+                                <div class="fw-semibold">$1000</div>
+                            </div>
+                            <div class="d-flex align-items-center g-10">
+                                <div class="">Signed Date</div>
+                                <div class="fw-semibold">John Smith</div>
+                            </div>
+                            <div class="d-flex align-items-center g-10">
+                                <div class="">Start Date</div>
+                                <div class="fw-semibold">Male</div>
+                            </div>
+                            <div class="d-flex align-items-center g-10">
+                                <div class="">End Date</div>
+                                <div class="fw-semibold">NV0001</div>
+                            </div>
+                            <div class="d-flex align-items-center g-10">
+                                <div class="">Position</div>
+                                <div class="fw-semibold">NV0001</div>
+                            </div>
+                            <div class="d-flex align-items-center g-10">
+                                <div class="">Contract type</div>
+                                <div class="fw-semibold">NV0001</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 max-height-350 mt-5">
+                <div class="bg-body-tertiary profile-wrapper h-100">
+                    <div class="d-flex justify-content-between align-items-center" style="margin-bottom: 24px">
+                        <h2>Settings</h2>
+                    </div>
+                    <div class="">
+                        <div class="d-flex flex-column g-20">
+                            <div class="d-flex align-items-center g-10">
+                                <div class="">Salary:</div>
+                                <div class="fw-semibold">$1000</div>
+                            </div>
+                            <div class="d-flex align-items-center g-10">
+                                <div class="">Signed Date</div>
+                                <div class="fw-semibold">John Smith</div>
+                            </div>
+                            <div class="d-flex align-items-center g-10">
+                                <div class="">Start Date</div>
+                                <div class="fw-semibold">Male</div>
+                            </div>
+                            <div class="d-flex align-items-center g-10">
+                                <div class="">End Date</div>
+                                <div class="fw-semibold">NV0001</div>
+                            </div>
+                            <div class="d-flex align-items-center g-10">
+                                <div class="">Position</div>
+                                <div class="fw-semibold">NV0001</div>
+                            </div>
+                            <div class="d-flex align-items-center g-10">
+                                <div class="">Contract type</div>
+                                <div class="fw-semibold">NV0001</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- app/views/layout/header/header.php
+++ app/views/layout/header/header.php
@@ -1,23 +1,23 @@
+<?php 
+$user = Session::data('user')['user'];
+?>
 <header class="">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Company</a>
+            <a class="navbar-brand" href="<?php echo $user['role'] == 'employee' ? _WEB_ROOT.'/dashboard' : _WEB_ROOT.'/admin/adminpanel' ?>">Company</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse justify-content-end" style="gap: 16px;" id="navbarSupportedContent">
                 <ul class="navbar-nav mb-2 mb-lg-0 ">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/dashboard">Home</a>
-                    </li>
                     <li class="nav-item d-flex justify-content-between align-items-center" style="gap: 20px">
                         <div class="section-employee-info d-flex gap-2" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             <div class="avatar">
                                 <img src="<?php echo _WEB_ROOT; ?>/public/assets/images/default-avatar.jpg" style="" />
                             </div>
                             <div class="info d-flex flex-column">
-                                <h3 class="username">Apollosuny</h3>
-                                <span class="user-role">Employee</span>
+                                <h3 class="username"><?php echo $user['username'] ? $user['username'] : 'Default name' ?></h3>
+                                <span class="user-role"><?php echo $user['role'] ? $user['role'] : 'Default role' ?></span>
                             </div>
                         </div>
                         <div>
